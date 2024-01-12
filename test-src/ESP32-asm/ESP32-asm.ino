5f09e4bc9144c71e41b34ccbcddb100ab6dce51f
Ricardo Mendes <mendes.r@gmx.com>
2024-01-12 22:11:27 +0000

    ADD stable asm

--- test-src/ESP32-asm/ESP32-asm.ino
+++ test-src/ESP32-asm/ESP32-asm.ino
@@ -1,7 +1,7 @@
 static const int LED_BUILTIN = 2;
 
 // put function declarations here:
-extern "C" int	assemblyAdd(int a, int b);
+extern "C" int	getMatrix(int a);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -13,19 +13,15 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  
-  volatile uint32_t i = 99;
   volatile uint32_t e = 0;
 
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
 
-  Serial.print("Valor de i: ");
-  Serial.println(i);
   Serial.print("Valor de e: ");
   Serial.println(e);
 
-  e = assemblyAdd(i, (uint32_t) 10);
+  e = getMatrix(4);
   Serial.print("Resultado: ");
   Serial.println(e);
   Serial.println();
