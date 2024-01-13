18557166a6d6e77b4b2aceab271bd78b1c9049e7
Ricardo Mendes <mendes.r@gmx.com>
2024-01-13 14:20:36 +0000

    ADD mask logic to sensor sim

--- test-src/ESP32-asm/ESP32-asm.ino
+++ test-src/ESP32-asm/ESP32-asm.ino
@@ -1,40 +1,51 @@
-static const int8_t LED = 18;
-
 static const int8_t LEVEL_LOW = 1;
 static const int8_t LEVEL_MIN = 2;
 static const int8_t LEVEL_MED = 3;
 static const int8_t LEVEL_MAX = 4;
 
+static const int8_t LED_LOW = 27;
+static const int8_t LED_MIN = 14;
+static const int8_t LED_MED = 12;
+static const int8_t LED_MAX = 13;
+
+static const int8_t MAX_SATES = 4;
+static const int8_t STATES[MAX_SATES] = {LED_LOW, LED_MIN, LED_MED, LED_MAX};
+
 extern "C" int	getMask(int a);
+volatile uint8_t mask = 1;
 
 void setup() {
   Serial.begin(9600);
 
   // initialize digital pin LED as an output.
-  pinMode(LED, OUTPUT);
+  pinMode(2, OUTPUT); // debug
+  pinMode(LED_LOW, OUTPUT);
+  pinMode(LED_MIN, OUTPUT);
+  pinMode(LED_MED, OUTPUT);
+  pinMode(LED_MAX, OUTPUT);
 }
 
 void loop() {
-  volatile uint8_t mask = 1;
+  digitalWrite(2, HIGH); // debug
+  delay(1000);
 
-  digitalWrite(LED, HIGH);  
-  delay(1000);                      
-
-  mask = getMask(LEVEL_MED);
+  mask = getMask(LEVEL_LOW);
   implementMask(mask);
 
-  digitalWrite(LED, LOW);   
+  digitalWrite(2, LOW); // debug
   delay(1000);                      
 }
 
 void implementMask(int8_t mask)
 {
   int n_bit = 0;
-  while (n_bit < 8) {
+  while (n_bit < MAX_SATES) {
     if (mask & 0x01) {
+      digitalWrite(STATES[n_bit], HIGH);
       Serial.print("1");
     }
     else {
+      digitalWrite(STATES[n_bit], LOW);
       Serial.print("0");
     }
 
