423a63cce689de32f9a94fe29bfa30709519b22e
Ricardo Mendes <mendes.r@gmx.com>
2024-01-13 17:57:53 +0000

    UPDATE control unit failure sim

--- src/control-unit/control-unit.ino
+++ src/control-unit/control-unit.ino
@@ -1,6 +1,10 @@
-static const uint8_t PUMP_1 = 18;
-static const uint8_t PUMP_CONTROL_1 = 19;
-static const uint8_t PUMP_STATE_1 = 21;
+static const uint8_t PUMP_1 = 13;
+static const uint8_t PUMP_STATE_1 = 12;
+static const uint8_t PUMP_CONTROL_1 = 14;
+
+static const uint8_t PUMP_2 = 26;
+static const uint8_t PUMP_STATE_2 = 25;
+static const uint8_t PUMP_CONTROL_2 = 27;
 
 
 void setup() {
@@ -9,16 +13,22 @@ void setup() {
   pinMode(PUMP_1, OUTPUT);
   pinMode(PUMP_STATE_1, OUTPUT);
   pinMode(PUMP_CONTROL_1, INPUT);
+
+  pinMode(PUMP_2, OUTPUT);
+  pinMode(PUMP_STATE_2, OUTPUT);
+  pinMode(PUMP_CONTROL_2, INPUT);
 }
 
 void loop() {
   digitalWrite(PUMP_1, HIGH);
+  digitalWrite(PUMP_2, HIGH);
 
   Serial.println("Checking pumps");
   updatePumpFailureState();
   delay(1000);
 
   digitalWrite(PUMP_1, LOW);
+  digitalWrite(PUMP_2, LOW);
   delay(1000);  
 }
 
@@ -30,5 +40,13 @@ void updatePumpFailureState()
   } else {
     Serial.println("Pump 1 FAILURE");
     digitalWrite(PUMP_STATE_1, LOW);
+  }
+
+    if (digitalRead(PUMP_CONTROL_2)){
+    Serial.println("Pump 2 is functional");
+    digitalWrite(PUMP_STATE_2, HIGH);
+  } else {
+    Serial.println("Pump 2 FAILURE");
+    digitalWrite(PUMP_STATE_2, LOW);
   }
 }
